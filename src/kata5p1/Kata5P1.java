84252e38d908ad03498e90fdae43326bd5aa5b93
Marco <marco_nehuen@hotmail.com>
2021-12-29 22:49:10 +0000

    Kata5P1 version 1

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,12 @@
+
+package kata5p1;
+
+public class Kata5P1 {
+
+    
+    public static void main(String[] args) {
+       SelectApp base = new SelectApp();
+       base.selectAll();
+    }
+    
+}
