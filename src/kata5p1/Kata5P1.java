edcde68380a8b97bea78dc1abaff38365c7c5568
Marco <marco_nehuen@hotmail.com>
2021-12-29 23:05:57 +0000

    Kata5P1 version 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -38,9 +38,26 @@ public class Kata5P1 {
             System.out.println(e.getMessage());
         }
     }
-
+    public static void createNewTable() {
+        // Cadena de conexión SQLite
+        String url = "jdbc:sqlite:KATA5.db";
+        // Instrucción SQL para crear nueva tabla
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " Mail text NOT NULL);";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement()) {
+            // Se crea la nueva tabla
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     public static void main(String[] args) {
        selectAll();
+       createNewTable();
     }
     
 }
